8528b6ce44b37120d8ba2c2323b51ed59c394585
Sam <47493725+Vuurvos1@users.noreply.github.com>
2024-08-18 20:17:42 +0200

    Upgrade to svelte 5

--- packages/lucide-svelte/scripts/exportTemplate.mjs
+++ packages/lucide-svelte/scripts/exportTemplate.mjs
@@ -12,25 +12,27 @@ ${getJSBanner()}
 import Icon from '../Icon.svelte';
 import type { IconNode, IconProps } from '../types.js';
 
-type $$Props = IconProps;
+let { children, ...restProps}: IconProps = $props();
 
 const iconNode: IconNode = ${JSON.stringify(children)};
-
-/**
- * @component @name ${componentName}
- * @description Lucide SVG icon component, renders SVG Element with children.
- *
- * @preview ![img](data:image/svg+xml;base64,${svgBase64}) - https://lucide.dev/icons/${iconName}
- * @see https://lucide.dev/guide/packages/lucide-svelte - Documentation
- *
- * @param {Object} props - Lucide icons props and any valid SVG attribute
- * @returns {FunctionalComponent} Svelte component
- * ${deprecated ? `@deprecated ${deprecationReason}` : ''}
- */
 </script>
 
-<Icon name="${iconName}" {...$$props} iconNode={iconNode}>
-  <slot/>
+<!--
+@component @name ${componentName}
+@description Lucide SVG icon component, renders SVG Element with children.
+
+@preview ![img](data:image/svg+xml;base64,${svgBase64}) - https://lucide.dev/icons/${iconName}
+@see https://lucide.dev/guide/packages/lucide-svelte - Documentation
+
+@param {IconProps} props - Lucide icons props and any valid SVG attribute
+@returns {FunctionalComponent} Svelte component
+${deprecated ? `@deprecated ${deprecationReason}` : ''}
+-->
+
+<Icon name="${iconName}" {...restProps} iconNode={iconNode}>
+  {#if children}
+    {@render children()}
+  {/if}
 </Icon>
 `;
 };
--- packages/lucide-svelte/src/Icon.svelte
+++ packages/lucide-svelte/src/Icon.svelte
@@ -1,45 +1,41 @@
 <script lang="ts">
-  import defaultAttributes from './defaultAttributes'
-  import type { IconNode } from './types';
+  import defaultAttributes from './defaultAttributes';
+  import type { IconProps } from './types';
 
-  export let name: string | undefined = undefined
-  export let color = 'currentColor'
-  export let size: number | string = 24
-  export let strokeWidth: number | string = 2
-  export let absoluteStrokeWidth: boolean = false
-  export let iconNode: IconNode = []
+  let {
+    name = undefined,
+    color = 'currentColor',
+    size = 24,
+    strokeWidth = 2,
+    absoluteStrokeWidth = false,
+    iconNode = [],
+    class: classes,
+    children,
+    ...restProps
+  }: IconProps = $props();
 
-  const mergeClasses = <ClassType = string | undefined | null>(
-    ...classes: ClassType[]
-  ) => classes.filter((className, index, array) => {
-      return Boolean(className) && array.indexOf(className) === index;
-    })
-    .join(' ');
-
+  const mergeClasses = <ClassType = string | undefined | null,>(...classes: ClassType[]) =>
+    classes
+      .filter((className, index, array) => {
+        return Boolean(className) && array.indexOf(className) === index;
+      })
+      .join(' ');
 </script>
 
 <svg
   {...defaultAttributes}
-  {...$$restProps}
+  {...restProps}
   width={size}
   height={size}
   stroke={color}
-  stroke-width={
-    absoluteStrokeWidth
-      ? Number(strokeWidth) * 24 / Number(size)
-      : strokeWidth
-  }
-  class={
-    mergeClasses(
-      'lucide-icon',
-      'lucide',
-      name ? `lucide-${name}`: '',
-      $$props.class
-    )
-  }
+  stroke-width={absoluteStrokeWidth ? (Number(strokeWidth) * 24) / Number(size) : strokeWidth}
+  class={mergeClasses('lucide-icon', 'lucide', name ? `lucide-${name}` : '', classes)}
 >
   {#each iconNode as [tag, attrs]}
-    <svelte:element this={tag} {...attrs}/>
+    <svelte:element
+      this={tag}
+      {...attrs}
+    />
   {/each}
-  <slot />
+  {@render children()}
 </svg>
